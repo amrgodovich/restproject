62622fed92d166873f51d07d746406d5b06dda40
amrgodovich <amr.godovic@gmail.com>
2025-11-18 09:58:03 +0200

    basic auth

--- project/settings.py
+++ project/settings.py
@@ -44,7 +44,15 @@ INSTALLED_APPS = [
 restframeworks={
 }
 
-REST_FRAMEWORK = {}
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
